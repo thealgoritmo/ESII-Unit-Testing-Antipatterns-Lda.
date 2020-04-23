62f08573786d80cbe29f6f52bce88efb907c0dcd
sr.jveiga <veiga757@gmail.com>
2020-04-23 13:03:09 +0100

    first commit

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+    private String name;
+    private int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData(){
+        if(id == -1) throw new NullPointerException();
+        return name + String.valueOf(id);
+    }
+}
---
+++ tests/PersonTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    public void testGettersPerson() {
+        Person p = new Person("Veiga", 987654321);
+        assertEquals(p.getName(), p.getName());
+        assertEquals(p.getId(), p.getId());
+    }
+
+    @Test
+    public void readFile() throws IOException {
+        var fileLocation = "C:\\Users\\sr.jveiga\\Desktop\\test.txt";
+        FileReader fr = new FileReader(fileLocation);
+        int i;
+        String s = "";
+        while ((i=fr.read()) != -1)
+            s += (char) i;
+        assertEquals("I'm a file ready to be read.", s);
+    }
+
+    @Test
+    public void testGetData() {
+        Person p = new Person("Veiga", 987654321);
+        assertEquals("Veiga987654321", p.getData());
+    }
+
+}
